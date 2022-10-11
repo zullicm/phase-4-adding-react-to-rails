76546ee066f8a84121d96c085c3134e66ee27fff
Christopher Marzulli <chriszulli15@gmail.com>
2022-10-11 13:39:53 -0400

    commit tree ed7bfd10ae0a26ff4f6ae36bd3f6e9cc9e021866

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    fetch("/movies")
+      .then((r) => r.json())
+      .then((movies) => console.log(movies));
+  }, []);
+
+  return <h1>Hello from React!</h1>;
+}
+
+export default App;
